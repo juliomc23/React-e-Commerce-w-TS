28a8075ef4a4d6faeb698a6dd30e309839c9af59
juliomc23 <julito2302mc@gmail.com>
2022-08-05 09:58:00 +0200

    products & product css-basic logic implemented

--- src/components/Header/ts/HeaderComponent.tsx
+++ src/components/Header/ts/HeaderComponent.tsx
@@ -1,27 +1,22 @@
-import { useContext } from "react";
-import { cartContext } from "../../../context/GlobalContext";
-
 import CartWishHComponent from "./CartWishHComponent";
 import LoginHComponent from "./LoginHComponent";
-import NavbarHComponent  from "./NavbarHComponent";
+import NavbarHComponent from "./NavbarHComponent";
 import SearchbarHComponent from "./SearchbarHComponent";
 
 import '../css/HeaderComponent.css'
 import SliderHComponent from "./SliderHComponent";
+import { Link } from "react-router-dom";
 
 
 export default function HeaderComponent() {
 
-  const {counter} = useContext(cartContext)
-  const {setCounter} = useContext(cartContext)
-
   return (
     <header className="header__div">
       <div>
-        <LoginHComponent /> 
+        <LoginHComponent />
       </div>
       <div className="div__componentsheader">
-        <img src="../../.././img/icons/logo_nike.png" alt="nike logo" className="img__logoheader"/>
+        <Link to='/'><img src="../../.././img/icons/logo_nike.png" alt="nike logo" className="img__logoheader" /></Link>
         <NavbarHComponent />
         <SearchbarHComponent />
         <CartWishHComponent />
